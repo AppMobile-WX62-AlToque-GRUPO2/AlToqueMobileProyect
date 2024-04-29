2d81a7677e282aef0bc058803324ef9de549d060
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2024-04-28 21:10:13 -0500

    feat(post): create base template Posting Activity for customer

--- app/src/main/java/com/example/altoque/IU/CustomerCreatePost.kt
+++ app/src/main/java/com/example/altoque/IU/CustomerCreatePost.kt
@@ -3,33 +3,34 @@ package com.example.altoque.IU
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.altoque.R
 
-class MainActivity : AppCompatActivity() {
+class CustomerCreatePost : AppCompatActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_customer_create_post)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         
-        val btToCreate = findViewById<Button>(R.id.btToCreate)
-        btToCreate.setOnClickListener{
-            redirectToCreationEnvironment()
+        val btPost = findViewById<Button>(R.id.btPost)
+        btPost.setOnClickListener{
+            Toast.makeText(this, "Se creo correctamente la publicación", Toast.LENGTH_LONG).show()
+            redirectToHome()
         }
-        
     }
     
-    fun redirectToCreationEnvironment(){
-        val intent = Intent(this, CustomerCreatePost::class.java)
+    private fun redirectToHome() {
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 }
