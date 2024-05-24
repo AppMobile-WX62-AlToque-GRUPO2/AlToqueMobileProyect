71b4b31fe704399dd4feea74bab93c442e07fa9a
Fiorella <U202120418@upc.edu.pe>
2024-05-24 16:06:57 -0500

    update(notifications): consume the API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+
 }
 
 android {
@@ -42,6 +43,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/altoque/IU/SpecialistNotification.kt
+++ app/src/main/java/com/example/altoque/IU/SpecialistNotification.kt
@@ -1,45 +1,66 @@
 package com.example.altoque.IU
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentManager
 import com.example.altoque.R
-import com.example.altoque.repository.PopupFragment
+import com.example.altoque.adapter.NotificationAdapter
+import com.example.altoque.models.Notification
+import com.example.altoque.networking.AlToqueService
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class SpecialistNotification : AppCompatActivity(),PopupFragment.OnNotificationRejectedListener {
+class SpecialistNotification : AppCompatActivity() {
+
+    lateinit var notifications : List<Notification>
+    lateinit var notificationAdapter: NotificationAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_specialist_notification)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constrain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val fragmentManager: FragmentManager = supportFragmentManager
-
-        findViewById<CardView>(R.id.cvNotification).setOnClickListener {
-            showPopup(fragmentManager)
-        }
-
-        findViewById<CardView>(R.id.cvNotification2).setOnClickListener {
-            showPopup(fragmentManager)
-        }
+        notificationAdapter = NotificationAdapter(emptyList())
+        loadNotification()
 
     }
 
-    private fun showPopup(fragmentManager: FragmentManager) {
-        val popupFragment = PopupFragment()
-        popupFragment.show(fragmentManager, "popup")
+    private fun loadNotification() {
+
+            val rvSpecialistNoti = findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.rvSpecialistNotification)
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://altoquebackendapi.onrender.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val service = retrofit.create(AlToqueService::class.java)
+
+            val request = service.getNotifications()
+
+            request.enqueue(object : retrofit2.Callback<List<Notification>> {
+                override fun onResponse(call: Call<List<Notification>>, response: retrofit2.Response<List<Notification>>) {
+                    if (response.isSuccessful) {
+                        notifications = response.body()!!
+                        notificationAdapter = NotificationAdapter(notifications)
+                        rvSpecialistNoti.adapter = notificationAdapter
+                        rvSpecialistNoti.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this@SpecialistNotification)
+                    }
+                }
+
+                override fun onFailure(call: Call<List<Notification>>, t: Throwable) {
+                    println("Error")
+                }
+            })
     }
 
-    override fun onNotificationRejected() {
-        findViewById<CardView>(R.id.cvNotification).visibility = View.GONE
-    }
 }
