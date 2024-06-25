3d52ebdf290d2f5e238f7fd6bd48feba082c932f
Jack Arana <arack.dev@gmail.com>
2024-06-24 19:33:08 -0500

    feat: add fragment closure function

--- app/src/main/java/com/example/altoque/IU/SpecialistPublicationDetailActivity.kt
+++ app/src/main/java/com/example/altoque/IU/SpecialistPublicationDetailActivity.kt
@@ -1,7 +1,10 @@
 package com.example.altoque.IU
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
@@ -28,6 +31,7 @@ import kotlinx.coroutines.launch
 
 class SpecialistPublicationDetailActivity : AppCompatActivity(), MessageFragment.OnNotificationRejectedListener {
 
+    private lateinit var handler: Handler
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,9 +65,25 @@ class SpecialistPublicationDetailActivity : AppCompatActivity(), MessageFragment
 
         findViewById<Button>(R.id.btnSpePubDetAccept).setOnClickListener {
             showMessage(fragmentManager)
+            startTimer()
         }
     }
-
+    private fun startTimer() {
+        handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            closeMessage()
+            redirectToHome()
+        }, 3000)
+    }
+    private fun closeMessage() {
+        findViewById<CardView>(R.id.cvSuccessMessage).visibility = View.GONE
+    }
+    private fun redirectToHome() {
+        val intent = Intent(this, MenuExpertActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+        finish()
+    }
     private fun loadPostDetails(postId: String) {
 
         val postService = RetrofitClient.createService<PostService>()
